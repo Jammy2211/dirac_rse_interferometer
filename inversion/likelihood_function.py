minor bug fix to make the code runs note that the chi_squared calculation is wrong and not fixed: https://pyautolens.slack.com/archives/C0825F8RHP0/p1738325488696129

inversion/likelihood_function.py
@@ -129,13 +129,13 @@ https://github.com/Jammy2211/PyAutoArray/blob/main/autoarray/inversion/inversion
 
 This uses the `w_tilde` matrix your previous task converted to JAX.
 """
-w_tilde = dataset.w_tilde
+w_tilde = dataset.w_tilde.w_matrix
 
 curvature_matrix = likelihood_function_funcs.curvature_matrix_via_w_tilde_from(
     w_tilde=w_tilde, mapping_matrix=mapping_matrix
 )
 
-"""
+r"""
 __JAX Function 3__
 
 A regularization matrix is next applied to the `curvature_matrix`, the calculation of which requires JAX-ifying.
@@ -237,7 +237,7 @@ chi_squared_term_2 = -np.multiply(2.0, np.dot(mapping_matrix, dirty_image)) # Ne
 
 chi_squared = chi_squared_term_1 + chi_squared_term_2
 
-"""
+r"""
 __JAX Function 7__
 
 The second term, $s^{T} H s$, corresponds to the $\lambda $G_{\rm L}$ regularization term we added to our merit 
@@ -256,7 +256,7 @@ regularization_term = np.matmul(
 
 print(regularization_term)
 
-"""
+r"""
 __JAX Function 8__
 
 Up to this point, it is unclear why we chose a value of `regularization_coefficient=1.0`. 